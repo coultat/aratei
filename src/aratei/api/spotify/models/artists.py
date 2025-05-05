e0e49d979f64abe88b2c9a17f5d78aad0945ab0b
Felipe Míguez Kautzmann <fkautzmann@immomio.de>
2025-05-05 13:07:36 +0200

    feat: add client get_artist

---
+++ src/aratei/api/spotify/models/artists.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel, Field, HttpUrl, RootModel, ConfigDict
+
+class ExternalURLs(BaseModel):
+    spotify: HttpUrl = Field(alias="spotify", description="Spotify URL")
+
+class Followers(BaseModel):
+    href: HttpUrl | None = Field(alias="href", description="Followers HREF")
+    total: int = Field(alias="total", description="Total number of followers")
+
+class Genres(RootModel):
+    root: list[str]
+
+class ArtistImage(BaseModel):
+    height: int = Field(alias="height", description="Image height")
+    width: int = Field(alias="width", description="Image width")
+    url: HttpUrl = Field(alias="url", description="Image URL")
+
+
+class Artist(BaseModel):
+    model_config = ConfigDict(model_validate=True, validate_by_alias=True)
+    external_urls: ExternalURLs = Field(alias="external_urls", description="External URLs")
+    followers: Followers
+    genres: Genres | None = Field(default=None, alias="genres", description="Genres")
+    href: HttpUrl = Field(alias="href", description="Artist URL")
+    artist_id: str = Field(alias="id", description="Artist ID")
+    images: list[ArtistImage]
+    name: str = Field(alias="name", description="Artist name")
+    popularity: int = Field(alias="popularity", description="Artist popularity")
+    type: str = Field(alias="type", description="Type of artist")
+    uri: str = Field(alias="uri", description="Artist URI")
---
+++ src/aratei/api/spotify/models/config.py
@@ -0,0 +1,47 @@
+from pydantic import BaseModel, Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import Path
+from typing import Self
+
+env_file = Path(__file__).parent.parent.parent.parent.parent.parent / "default.env"
+
+
+from typing import ClassVar, Self
+#from warnings import deprecated
+
+from pydantic_settings import BaseSettings
+
+
+class InternalCachedBaseSettings(BaseSettings):
+    """Base class for settings with caching without vault access. Exists to prevent circular imports."""
+
+    _cached_instance: ClassVar[Self | None] = None
+
+    @classmethod
+    def load(cls) -> Self:
+        """Get settings from the usual places - env, secret files. Caches the result indefinitely."""
+        # can't use cache decorator because of typing issues so we create our own cache
+        if cls._cached_instance is None:
+            cls._cached_instance = cls()
+        return cls._cached_instance
+
+
+class SpotifyConfig(InternalCachedBaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=env_file,
+        env_file_encoding="utf-8",
+        env_nested_delimiter="__",
+        extra="ignore"
+    )
+    client_id: str = Field(..., validation_alias="SPOTIFY_CLIENT_ID")
+    client_secret: str = Field(..., validation_alias="SPOTIFY_CLIENT_SECRET")
+    auth_url: str = Field(..., validation_alias="SPOTIFY_AUTH_URL")
+    timeout: int = Field(..., validation_alias="SPOTIFY_TIMEOUT")
+    base_url: str = Field(..., validation_alias="SPOTIFY_BASE_URL")
+
+    @property
+    def credentials(self):
+        return f"{self.client_id}:{self.client_secret}"
+
+
+spotify_config = SpotifyConfig.load()
