2210271184e78e5b1904c3176c5b1d4fbe27d91b
Felipe Míguez Kautzmann <fkautzmann@immomio.de>
2025-05-06 10:15:35 +0200

    fastapi_app configured

--- src/aratei/api/api_models/fastapi_model.py
+++ src/aratei/api/api_models/fastapi_model.py
@@ -1,7 +1,7 @@
-from pydantic import BaseModel
+from src.aratei.api.api_models.base_settings import InternalCachedBaseSettings
 
 
-class AppConfig(BaseModel):
+class AppConfig(InternalCachedBaseSettings):
     """
     App config for aratei. Can be used as a standalone config or as part of another config.
     General settings of the application belong here.
--- src/aratei/api/app/fastapi_config.py
+++ src/aratei/api/app/fastapi_config.py
@@ -5,7 +5,7 @@ from src.aratei.api.api_models.fastapi_model import AppConfig
 from starlette.responses import Response
 
 
-def create_fastapi_app(app_config: AppConfig.load(), version: str) -> FastAPI:
+def create_fastapi_app(app_config: AppConfig, version: str) -> FastAPI:
     """
     Create fastapi app from appconfig and version.
     Also sets an exception handler and adds a redirect from / to /docs.
