5626b96c613f52246cb8576368b895ddca0dd9c3
Felipe Míguez Kautzmann <fkautzmann@immomio.de>
2025-05-06 10:50:22 +0200

    Comments deleted from base_settings

--- src/aratei/api/api_models/base_settings.py
+++ src/aratei/api/api_models/base_settings.py
@@ -4,14 +4,12 @@ from pydantic_settings import BaseSettings
 
 
 class InternalCachedBaseSettings(BaseSettings):
-    """Base class for settings with caching without vault access. Exists to prevent circular imports."""
 
     _cached_instance: ClassVar[Self | None] = None
 
     @classmethod
     def load(cls) -> Self:
         """Get settings from the usual places - env, secret files. Caches the result indefinitely."""
-        # can't use cache decorator because of typing issues so we create our own cache
         if cls._cached_instance is None:
             cls._cached_instance = cls()
         return cls._cached_instance  # type: ignore[return-value]
